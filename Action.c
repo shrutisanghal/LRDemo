38d9683197d26d82b7b04c1a25beb9c8eeb3df66
Shruti Sanghal <shruti.sanghal@gmail.com>
2020-06-13 19:02:19 -0700

    This is my first commit

---
+++ Action.c
@@ -0,0 +1,15 @@
+
+
+	Action()
+{
+		
+		web_custom_request("web_custom_request",
+		"URL=http://blazedemo.com",
+		"Method=GET",
+		"TargetFrame=",
+		"Resource=0",
+		"Referer=",
+		"Body=",
+		LAST);
+	return 0;
+}
